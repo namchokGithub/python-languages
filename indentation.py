0495abeeee2d5dd56bf3bd9e07bc3c240c153452
namchokGithub <pech4751@gmail.com>
2019-11-14 00:44:23 +0700

    indentation

---
+++ indentation.py
@@ -0,0 +1,8 @@
+print('Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important. Python uses indentation to indicate a block of code.')
+
+if 5 > 2:
+    print('Yes')
+
+# Error
+# if 5 > 2:
+# print('Yes') 
