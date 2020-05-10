6d7339f3fcbd3b5cbff8c6e79654dc834a95a0e4
Namchok <pech4751@gmail.com>
2020-05-10 22:20:59 +0700

    String Format

--- practice/string.py
+++ practice/string.py
@@ -64,3 +64,18 @@ print(c)
 
 c = a + " " + b
 print(c)
+
+# String Format
+age = 35
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.93
+myorder = "I want {} pieces of item {} for {} dollars"
+print(myorder.format(quantity, itemno, price))
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}"
+print(myorder.format(quantity, itemno, price))
+
