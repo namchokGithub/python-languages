3c7df23d4831a9e798e28e11c03a36c44b47650f
Namchok <pech4751@gmail.com>
2020-05-10 21:57:02 +0700

    Casting

---
+++ practice/casting.py
@@ -0,0 +1,30 @@
+# Python Casting
+
+# Integers
+x = int(1) 
+y = int(2.7)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+# Floats
+x = float(1)
+y = float(2.6)
+z = float("3")
+w = float("4.2")
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+# Strings
+x = str("s1")
+y = str(2)
+z = str(3.0)
+
+print(x)
+print(y)
+print(z)
