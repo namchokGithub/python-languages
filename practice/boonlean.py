7919ade02e8c1529d70da08148c5ecfb5dc61233
Namchok <pech4751@gmail.com>
2020-05-11 23:47:06 +0700

    Evaluate Values and Variables

--- practice/boonlean.py
+++ practice/boonlean.py
@@ -13,3 +13,14 @@ if b > a:
     print("b is greater than a")
 else:
     print("b is not greater than a")
+
+# Evaluate Values and Variables
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
