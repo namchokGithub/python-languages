14cf6b662467f967d13b73f534991f05f40bfee1
Namchok <pech4751@gmail.com>
2020-05-11 23:45:29 +0700

    Boolean Values

---
+++ practice/boonlean.py
@@ -0,0 +1,15 @@
+# Python Booleans
+# https://www.w3schools.com/python/python_booleans.asp
+
+# Boolean Values
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+ 
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
