8da54e4adc630118c823772f59273488e2822814
Namchok <pech4751@gmail.com>
2020-05-12 20:56:10 +0700

    Python Comparison Operators

--- practice/operator.py
+++ practice/operator.py
@@ -3,15 +3,15 @@
 
 print("=== Python Arithmetic Operators ===")
 
-print(1+2)
-print(1+-2)
-print(1-2)
-print(1-+3)
-print(2*3)
-print(1/5)
-print(2%3)
-print(2**2)
-print(12//5)
+# print(1+2)
+# print(1+-2)
+# print(1-2)
+# print(1-+3)
+# print(2*3)
+# print(1/5)
+# print(2%3)
+# print(2**2)
+# print(12//5)
 
 print("=== Python Assignment Operators ===")
 
@@ -33,9 +33,23 @@ x=5
 x>>=3
 x<<=3
 
-print(x)
+# print(x)
 
 print("=== Python Comparison Operators ===")
+
+if 2==3: 
+    print(True)
+if 2!=3: 
+    print(True)
+if 2>3: 
+    print(True)
+if 2<3: 
+    print(True)
+if 2>=3: 
+    print(True)
+if 2<=3: 
+    print(True)
+
 print("=== Python Logical Operators ===")
 print("=== Python Identity Operators ===")
 print("=== Python Membership Operators ===")
