8b829f9683dd6c1ecf886596e0a293e99bfd4a37
Namchok <pech4751@gmail.com>
2020-05-12 21:02:47 +0700

    Python Identity Operators

--- practice/operator.py
+++ practice/operator.py
@@ -64,8 +64,15 @@ if not(2==3 and 3==3):
 print("=== Python Identity Operators ===")
 
 x = ['van', 'bmw']
+y = ['van', 'bmw']
+z = x
+print(x is z) # returns True because z is the same object as x
+print(x is y) # returns False because x is not the same object as y, even if they have the same content
+print(x == y) # to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
+
+print("=== Python Membership Operators ===")
+
+x = ['van', 'bmw']
 print('banana' in x)
 print('van' in x)
 print('banana' not in x)
-
-print("=== Python Membership Operators ===")
