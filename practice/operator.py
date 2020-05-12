8463dbbc7e9a8bb38721378053e5cc544128ea64
Namchok <pech4751@gmail.com>
2020-05-12 20:47:49 +0700

    Python Arithmetic Operators

--- practice/operator.py
+++ practice/operator.py
@@ -2,6 +2,17 @@
 # https://www.w3schools.com/python/python_operators.asp
 
 print("=== Python Arithmetic Operators ===")
+
+print(1+2)
+print(1+-2)
+print(1-2)
+print(1-+3)
+print(2*3)
+print(1/5)
+print(2%3)
+print(2**2)
+print(12//5)
+
 print("=== Python Assignment Operators ===")
 print("=== Python Comparison Operators ===")
 print("=== Python Logical Operators ===")
