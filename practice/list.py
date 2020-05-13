bc0566424f38639f4be330e362c04e8569ba5da4
Namchok <pech4751@gmail.com>
2020-05-13 16:40:34 +0700

    Change Item Value

--- practice/list.py
+++ practice/list.py
@@ -16,3 +16,13 @@ print(thislist[-1])
 # Range of Indexes
 thislist = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
 print(thislist[2:5])
+print(thislist[:4])
+print(thislist[2:])
+
+print("=== Range of Nagative Indexes ===")
+print(thislist[-4:-1])
+
+print("=== Change Item Value ===")
+
+thislist[1] = "blackcurrent"
+print(thislist)
