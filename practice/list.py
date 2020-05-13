9c0e721b8db989fbee89c9b83cff217a973e5fb1
Namchok <pech4751@gmail.com>
2020-05-13 16:05:33 +0700

    Access Item

---
+++ practice/list.py
@@ -0,0 +1,17 @@
+# Python Lists
+# https://www.w3schools.com/python/python_lists.asp
+
+# Example
+thislist = ["apple", "banana", "cherry"]
+
+# Print lists
+print(thislist)
+
+print("=== Access Item ===")
+print(thislist[1])
+
+# Nagative indexing
+print(thislist[-1])
+
+# Range of Indexes
+print()
