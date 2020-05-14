2f71296c71fc687b1308a9683c24c1f977b9665b
Namchok <pech4751@gmail.com>
2020-05-14 16:58:40 +0700

    Add and remove Items

--- practice/sets.py
+++ practice/sets.py
@@ -15,3 +15,28 @@ for x in thisset:
 print("banana" in thisset) # True
 print("mango" in thisset) # False
 
+# once a set is created, you cannot change its items, but you can add new items.
+print("=== Add items ===")
+
+thisset.add("orange")
+print(thisset)
+
+# Update sets
+# add multi items
+set1 = {"apple", "mango", "banana"}
+set1.update(["a", "b", "c"])
+print(set1)
+
+print("=== Remove items ===")
+print(thisset)
+thisset.remove("banana")
+print(thisset)
+thisset.discard("cherry")
+print(thisset)
+
+print(set1)
+set1.pop()
+print(set1)
+set1.clear()
+
+del set1
