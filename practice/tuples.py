50b45c40622651f5ee8feacc653f88f8397c74ce
Namchok <pech4751@gmail.com>
2020-05-14 16:35:34 +0700

    Access tuples

--- practice/tuples.py
+++ practice/tuples.py
@@ -4,3 +4,16 @@
 # Example
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+print("=== Access Tuple Items ===")
+
+print(thistuple[1])
+print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "kiwi", "mango", "melon")
+
+# Range of Indexes
+print(thistuple[2:4])
+# Range of Negative Indexes
+print(thistuple[-4:-2])
+
