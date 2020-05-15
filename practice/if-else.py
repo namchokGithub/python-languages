83b96bcc2be2a9546282693669514851dd280222
Namchok <pech4751@gmail.com>
2020-05-15 23:15:12 +0700

    Short hand

--- practice/if-else.py
+++ practice/if-else.py
@@ -23,3 +23,15 @@ elif a == b:
 else:
     print("=== a is greater than b")
 
+print("=== Short Hand if ===")
+
+# If you have only one statement to execute, you can put it on the same line as the if statement
+if a > b: print("a is greater than b")
+
+print("=== Short hand if else ===")
+
+# Put it all on the same line
+print("A") if a < b else print("B")
+
+# you can also have multiple else statements on the same line.
+print("A") if a > b else print("=") if a == b else print("B")
