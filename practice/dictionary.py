13ba1f876ec177609aee0841849446a05e1d3d0e
Namchok <pech4751@gmail.com>
2020-05-15 22:48:35 +0700

    loop through a dictionanry

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -23,3 +23,17 @@ print("=== Change Values ===")
 print(thisdict.get("year"))
 thisdict["year"] = 1999
 print(thisdict.get("year"))
+
+print("=== Loop Through a Dictionary ===")
+
+for x in thisdict:
+    print(x) # print keys
+
+for x in thisdict:
+    print(thisdict[x]) # print values
+
+for x in thisdict.values():
+    print(x) # print values
+
+for x, y in thisdict.items():
+    print(x, y) # print keys and values
