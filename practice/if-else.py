f0db023df242d61c3d37b7bf8a254e01b39e7009
Namchok <pech4751@gmail.com>
2020-05-15 23:09:54 +0700

    else and elif

---
+++ practice/if-else.py
@@ -0,0 +1,25 @@
+# Python If ... Else
+# https://www.w3schools.com/python/python_conditions.asp
+
+print("=== Elif ===")
+
+a = 33
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are aqual")
+
+print("=== Else ===")
+
+a = 300
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("=== a is greater than b")
+
