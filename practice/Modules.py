49e632bf2047f93d27e960d24f2b497ff448580f
Namchok <pech4751@gmail.com>
2020-09-20 22:59:30 +0700

    update python

--- practice/Modules.py
+++ practice/Modules.py
@@ -2,24 +2,23 @@
 # https://www.w3schools.com/python/python_modules.asp
 
 # Example Import module
-# import mymodule
-# mymodule.greeting("Namchok")
+import mymodule
+mymodule.greeting("Namchok")
 
 # Import variables
-# a = mymodule.person1["age"]
-# print(a)
+a = mymodule.person1["age"]
+print(a)
 
 # Re-naming a Module
-# import mymodule as ax
-# a = ax.person1["age"]
-# print(a)
+import mymodule as ax
+a = ax.person1["age"]
+print(a)
 
-# import platform
-
-# x = platform.system()
-# print(x)
-# x = dir(platform)
-# print(x)
+import platform
+x = platform.system()
+print(x)
+x = dir(platform)
+print(x)
 
 # Import From Module
 from mymodule import person1
