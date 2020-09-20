520ae68418d52a592801b380b10b959f2a92b780
Namchok <pech4751@gmail.com>
2020-09-20 22:52:45 +0700

    add import module and call method

---
+++ practice/Modules.py
@@ -0,0 +1,8 @@
+# Python Modules
+# https://www.w3schools.com/python/python_modules.asp
+
+# Example 
+import mymodule
+
+mymodule.greeting("Namchok")
+
---
+++ practice/mymodule.py
@@ -0,0 +1,3 @@
+# Example 
+def greeting(name):
+    print("Hello, " + name)
