fe0390e5d3fe6b3c70b216571f0e8cc22918876a
Namchok <pech4751@gmail.com>
2020-09-20 22:58:39 +0700

    import from module

--- practice/Modules.py
+++ practice/Modules.py
@@ -10,6 +10,18 @@
 # print(a)
 
 # Re-naming a Module
-import mymodule as ax
-a = ax.person1["age"]
-print(a)
+# import mymodule as ax
+# a = ax.person1["age"]
+# print(a)
+
+# import platform
+
+# x = platform.system()
+# print(x)
+# x = dir(platform)
+# print(x)
+
+# Import From Module
+from mymodule import person1
+
+print (person1["age"])
