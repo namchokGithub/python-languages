b3087b9980a31b2cf27ccb82024fd3d2e1e5af8f
Namchok <pech4751@gmail.com>
2020-10-04 22:06:42 +0700

    simple command pip

---
+++ practice/PIP/pipsample.py
@@ -0,0 +1,18 @@
+# Python PIP
+# https://www.w3schools.com/python/python_pip.asp
+
+# Command Check if PIP is Installed
+print("pip --version")
+
+# Download a Package
+print("pip install camelcase")
+# import camelcase
+# c = camelcase.CamelCase()
+# txt = "hello world"
+# print(c.hump(txt))
+
+# Remove a Package
+print("pip uninstall camelcase")
+
+# List Packages
+print("pip list")
