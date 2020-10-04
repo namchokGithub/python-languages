1642ee5afd4f34b3196d5018db61464099d60468
Namchok <pech4751@gmail.com>
2020-10-04 21:20:09 +0700

    add match

---
+++ practice/match.py
@@ -0,0 +1,25 @@
+# Python Math
+# https://www.w3schools.com/python/python_math.asp
+
+# Built in math fuctions
+# The min() and max() functions can be used to find the lowest or highest value in an iterable
+y = min(5, 10, 15)
+x = min(2, 10, 15)
+print(x)
+print(y)
+
+# The match module
+import math
+x = math.sqrt(64)
+print(x)
+
+# The math.ceil() method rounds a number upwards to its nearest integer, 
+# and the math.floor() method rounds a number downwards to its nearest integer, and returns the result
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 1
+
+# PI
+x = math.pi
+print(x)
